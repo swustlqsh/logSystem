bcd30f84f566b0995493398e283b8c6adef8ebb2
刘娟娟 <1003512331@qq.com>
2016-06-28 17:14:29 +0800

    login

--- client/js/main.js
+++ client/js/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from '../views/App.vue'
 import List from '../views/List.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueResource);
 new Vue({
   el: 'body',
-  components: { App, List}
+  components: { App, List,Login}
 });
---
+++ client/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="center">
+        <form class="form-horizontal  col-md-offset-4 col-md-4">
+            <div class="form-group col-md-10">
+             <input type="email" class="form-control" id="email" placeholder="email">
+            </div>
+            <button type="submit" class="btn btn-default">登录</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: '日志管理'
+            }
+        }
+    }
+</script>

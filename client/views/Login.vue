8b5d90eb0e80e810aed26a26dd48a2e549c7b298
刘娟娟 <1003512331@qq.com>
2016-06-28 17:31:55 +0800

    login

--- client/views/Login.vue
+++ client/views/Login.vue
@@ -2,18 +2,23 @@
     <div class="center">
         <form class="form-horizontal  col-md-offset-4 col-md-4">
             <div class="form-group col-md-10">
-             <input type="email" class="form-control" id="email" placeholder="email">
+                <input type="email" class="form-control" id="email" placeholder="email">
             </div>
-            <button type="submit" class="btn btn-default">登录</button>
+            <a type="submit" class="btn btn-default" @click="login">登录</a>
         </form>
     </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
     export default {
         data () {
             return {
                 msg: '日志管理'
+            }
+        },
+        methods: {
+            login: ()=> {
+                console.log('login')
             }
         }
     }

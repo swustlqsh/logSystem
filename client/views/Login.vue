5933e09fe42bae10c821c28c30de9ee9080acfad
刘娟娟 <1003512331@qq.com>
2016-06-29 11:49:29 +0800

    路由及resource均ok

--- client/views/Login.vue
+++ client/views/Login.vue
@@ -4,7 +4,7 @@
             <div class="form-group col-md-10">
                 <input type="email" class="form-control" id="email" placeholder="email">
             </div>
-            <a type="submit" class="btn btn-default" @click="login(this)">登录</a>
+            <a type="submit" class="btn btn-default" @click="login()">登录</a>
         </form>
     </div>
 </template>
@@ -17,10 +17,9 @@
                 msg: '日志管理'
             }
         },
-        methods: {
-            login: (v)=> {
-                console.log(v);
-               v.$route.router.go({name:'list'});
+        methods:{
+            login(){
+                this.$router.go('list')
             }
         }
     }

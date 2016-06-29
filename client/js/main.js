13c9305b720b34c55eb6d204bfdbde936f2e6549
刘娟娟 <1003512331@qq.com>
2016-06-29 09:23:38 +0800

    router

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,29 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import App from '../views/App.vue'
+import List from '../views/List.vue'
+import Login from '../views/Login.vue'
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const app=Vue.extend({});
+
+const router=new VueRouter();
+router.map({
+  '/list':{component:List},
+  '/login':{component:Login}
+});
+router.start(app,'body');
+
+/*new Vue({
+  el: 'body',
+  components: { App, List,Login}
+});*/
+/*
+import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from '../views/App.vue'
 import List from '../views/List.vue'
 import Login from '../views/Login.vue'
@@ -9,3 +33,4 @@ new Vue({
   el: 'body',
   components: { App, List,Login}
 });
+*/

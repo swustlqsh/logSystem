8e6f7b8260ad24339cccdcfe27960e1023b1d4fb
刘娟娟 <1003512331@qq.com>
2016-06-29 09:30:20 +0800

    router

--- client/js/main.js
+++ client/js/main.js
@@ -1,36 +1,15 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
-import App from '../views/App.vue'
-import List from '../views/List.vue'
-import Login from '../views/Login.vue'
+import {configRouter} from './route-config'
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
 const app=Vue.extend({});
-
 const router=new VueRouter();
-router.map({
-  '/list':{component:List},
-  '/login':{component:Login}
-});
+
+configRouter(router);
+
 router.start(app,'body');
 
-/*new Vue({
-  el: 'body',
-  components: { App, List,Login}
-});*/
-/*
-import Vue from 'vue'
-import VueResource from 'vue-resource'
-import App from '../views/App.vue'
-import List from '../views/List.vue'
-import Login from '../views/Login.vue'
-
-Vue.use(VueResource);
-new Vue({
-  el: 'body',
-  components: { App, List,Login}
-});
-*/

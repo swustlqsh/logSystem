fbe3825771226c4741da71bb5f7b55fd3c1a020a
刘娟娟 <1003512331@qq.com>
2016-06-30 17:03:30 +0800

    team

--- manager/views/List.vue
+++ manager/views/List.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="list-group col-md-3 outBorder">
+    <div id="teamList" class="list-group col-md-3 outBorder">
         <button class="btn btn-info" @click="insert()" style="margin: 15px">添加部门</button>
         <a id="111111111111111111111111" class="list-group-item inBorder bg-grey" @click="activeChange('111111111111111111111111')">
             <span @click="activeChange('111111111111111111111111')" class="hand">topxgun</span>
-            </a>
+        </a>
         <a id='{{team._id}}' class="list-group-item inBorder" v-for="team in teams">
             <span @click="activeChange(team._id)" class="hand">{{team.name}}</span>
             <span class="pull-right text-info hand" @click="remove(team._id)">删除&nbsp;&nbsp;</span>
@@ -85,29 +85,56 @@
                         _this.find();
                     });
                     document.getElementById('confirm').addEventListener('click',()=>{
-                        let teamObj={_id:team._id,name:temp.value};
-                        _this.$http.post('http://localhost:1234/team/update',teamObj).then((res) => {
-                            if(res.data.code==200){
-                                _this.find();
-                                _this.findUsers();
-                            }
-                        },(err) => {
-                            console.log(err);
-                        });
+                        if(temp.value==''){
+                            alert('请填写部门名称！')
+                        }else{
+                            let teamObj={_id:team._id,name:temp.value};
+                            _this.$http.post('http://localhost:1234/team/update',teamObj).then((res) => {
+                                if(res.data.code==200){
+                                    _this.find();
+                                    _this.findUsers(this.activeTeam);
+                                }
+                            },(err) => {
+                                console.log(err);
+                            });
+                        }
                     })
                 }else{
                     alert('其他部门有编辑操作，请不要同时操作！')
                 }
             },
             insert(){
-                let teamObj={name:'默认部门'};
-                this.$http.post('http://localhost:1234/team/insert',teamObj).then((res)=>{
-                    if(res.data.code==200){
-                        this.find();
+                var newA=document.createElement('a');
+                newA.setAttribute('class','list-group-item inBorder');
+                newA.innerHTML="<input type='text' id='insertTemp'>" +
+                        "<span id='insertCancel' class='pull-right text-info hand'>取消&nbsp;&nbsp;</span>" +
+                        "<span id='insertConfirm' class='pull-right text-info hand'>确定&nbsp;&nbsp;</span>";
+                if(this.teams[0]&&this.teams[0]._id){
+                    let aFirst=document.getElementById(this.teams[0]._id);
+                    document.getElementById('teamList').insertBefore(newA,aFirst);
+                }else{
+                    document.getElementById('teamList').appendChild(newA);
+                }
+                document.getElementById('insertCancel').addEventListener('click',()=>{
+                    document.getElementById('teamList').removeChild(document.getElementById('teamList').children[2]);
+                    this.find();
+                });
+                document.getElementById('insertConfirm').addEventListener('click',()=>{
+                    let name=document.getElementById('insertTemp').value;
+                    if(!name){
+                        alert('请完善信息！')
+                    }else{
+                        let teamObj={name:name};
+                        this.$http.post('http://localhost:1234/team/insert',teamObj).then((res)=>{
+                            if(res.data.code==200){
+                                document.getElementById('teamList').removeChild(document.getElementById('teamList').children[2]);
+                                this.find();
+                            }
+                        },(err)=>{
+                            console.log(err);
+                        })
                     }
-                },(err)=>{
-                    console.log(err);
-                })
+                });
             },
             remove(teamId){
                 this.$http.delete('http://localhost:1234/team/'+teamId).then((res)=>{
@@ -180,41 +207,45 @@
                 this.findUsers(this.activeTeam)
             },
             updateUser(user){
-                let tds=document.getElementById(user._id).children;
-                let teams=this.teams;
-                let options='<option value="">--请选择--</option>';
-                for(let i of teams){
-                    if(user.team&&(i._id==user.team._id)){
-                        options+='<option selected value="'+i._id+'">'+i.name+'</option>'
-                    }else{
-                        options+='<option value="'+i._id+'">'+i.name+'</option>'
+                if(document.getElementById('userUpdateTd0')==undefined) {
+                    let tds = document.getElementById(user._id).children;
+                    let teams = this.teams;
+                    let options = '<option value="">--请选择--</option>';
+                    for (let i of teams) {
+                        if (user.team && (i._id == user.team._id)) {
+                            options += '<option selected value="' + i._id + '">' + i.name + '</option>'
+                        } else {
+                            options += '<option value="' + i._id + '">' + i.name + '</option>'
+                        }
                     }
+                    tds[0].innerHTML = "<input type='text' id='userUpdateTd0' value='" + user.name + "'>";
+                    tds[1].innerHTML = "<input type='text' id='userUpdateTd1' value='" + user.email + "'>";
+                    tds[2].innerHTML = '<select id="userUpdateTd2">' + options + '</select>';
+                    tds[4].innerHTML = '<button class="btn btn-info btn-sm" id="userUpdateConfirm">确定</button>&nbsp;' +
+                            '<button class="btn btn-info btn-sm" id="userUpdateCancel">取消</button>';
+                    document.getElementById('userUpdateConfirm').addEventListener('click', ()=> {
+                        let value0 = document.getElementById('userUpdateTd0').value;
+                        let value1 = document.getElementById('userUpdateTd1').value;
+                        let value2 = document.getElementById('userUpdateTd2').value;
+                        if (value0 && value1 && value2) {
+                            let userObj = {_id: user._id, name: value0, email: value1, team: value2};
+                            this.$http.post('http://localhost:1234/user/update', userObj).then((res)=> {
+                                if (res.data.code == 200) {
+                                    this.findUsers(this.activeTeam);
+                                }
+                            }, (err)=> {
+                                console.log(err);
+                            })
+                        } else {
+                            alert('请完善信息！')
+                        }
+                    });
+                    document.getElementById('userUpdateCancel').addEventListener('click', ()=> {
+                        this.findUsers(this.activeTeam);
+                    });
+                }else{
+                    alert('其他员工有编辑操作，请不要同时操作！')
                 }
-                tds[0].innerHTML="<input type='text' id='userUpdateTd0' value='"+user.name+"'>";
-                tds[1].innerHTML="<input type='text' id='userUpdateTd1' value='"+user.email+"'>";
-                tds[2].innerHTML='<select id="userUpdateTd2">' + options+ '</select>';
-                tds[4].innerHTML='<button class="btn btn-info btn-sm" id="userUpdateConfirm">确定</button>&nbsp;' +
-                        '<button class="btn btn-info btn-sm" id="userUpdateCancel">取消</button>';
-                document.getElementById('userUpdateConfirm').addEventListener('click',()=>{
-                    let value0=document.getElementById('userUpdateTd0').value;
-                    let value1=document.getElementById('userUpdateTd1').value;
-                    let value2=document.getElementById('userUpdateTd2').value;
-                    if(value0&&value1&&value2){
-                        let userObj={_id:user._id,name:value0,email:value1,team:value2};
-                        this.$http.post('http://localhost:1234/user/update',userObj).then((res)=>{
-                            if(res.data.code==200){
-                                this.findUsers(this.activeTeam);
-                            }
-                        },(err)=>{
-                            console.log(err);
-                        })
-                    }else{
-                        alert('请完善信息！')
-                    }
-                });
-                document.getElementById('userUpdateCancel').addEventListener('click',()=>{
-                    this.findUsers(this.activeTeam);
-                });
             },
             removeUser(userId){
                 this.$http.delete('http://localhost:1234/user/'+userId).then((res)=>{

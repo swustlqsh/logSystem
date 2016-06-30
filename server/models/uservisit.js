4f428dd7db7db75c243847adc5ada3884768b22c
刘娟娟 <1003512331@qq.com>
2016-06-30 18:16:35 +0800

    auth

--- server/models/uservisit.js
+++ server/models/uservisit.js
@@ -4,6 +4,7 @@ const ObjectId = Schema.Types.ObjectId;
 
 const UserVisitSchema = new Schema({
 	user_id:{type:ObjectId,ref:'User'},
+	team:{type:ObjectId,ref:'Team'},//被访问者所属团队
 	visit_id:{type:ObjectId,ref:'User'}
 });
 	

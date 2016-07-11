0d2576871668fa9cf57c0b7e71e65d4ed8420aec
刘娟娟 <1003512331@qq.com>
2016-07-11 18:17:14 +0800

    不可重复添加提示

--- client/services/dairy.js
+++ client/services/dairy.js
@@ -3,6 +3,7 @@
  */
 import {API_URL} from '../config'
 export default{
+    //添加日志
     insert(app,dairyObj,cb){
         app.$http.post(API_URL+'/dairy/insert/',dairyObj).then((res)=> {
             if(res.data.code==200){
@@ -13,5 +14,19 @@ export default{
         },(err)=>{
             console.log(err);
         });
+    },
+    //添加的时候看一下今天是否已经添加
+    insertDairy(app,userId,cb){
+        app.$http.get(API_URL+'/dairy/isInsert/'+userId).then((res)=>{
+            if(res.data.code==201){
+                cb();
+            }else if(res.data.code==200){
+
+            }else if(res.data.code==555){
+                console.log(res.data.data);
+            }
+        },(err)=>{
+            console.log(err);
+        })
     }
 }

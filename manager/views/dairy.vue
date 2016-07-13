c9a3eeabe934784116c6fe6acecd459598667dfd
刘娟娟 <1003512331@qq.com>
2016-07-13 14:50:44 +0800

    日志列表分页完毕

--- manager/views/dairy.vue
+++ manager/views/dairy.vue
@@ -10,7 +10,7 @@
         </div>
         <div class="col-lg-12">
             <div class="col-lg-3" v-for="item in list">
-            <div class="panel panel-default">
+            <div class="panel panel-info">
                 <div class="panel-heading">
                     <h3 class="panel-title">{{item.create_date|date}}</h3>
                 </div>
@@ -20,6 +20,12 @@
             </div>
         </div>
         </div>
+        <div class="col-lg-12" v-if="this.pagination.last_page>1">
+            <div class="pull-right">
+                <pagination :pagination="pagination" :callback="findDairyByUser" :offset="8"></pagination>
+            </div>
+        </div>
+
     </div>
 </template>
 <script type="text/ecmascript-6">
@@ -27,12 +33,19 @@
     export default{
         ready(){
             this.find(this.$route.params.userId);
-            this.findDairyByUser(this.$route.params.userId);
+            this.findDairyByUser();
         },
         data(){
             return{
                 user:{},//user
-                list:[]//user 日志
+                list:[],//user 日志
+                pagination: {
+                    total: 0, per_page: 16,
+                    from: 0, to: 1,
+                    current_page: 1,
+                    last_page: 1
+                },
+                paginate: 1
             }
         },
         methods:{
@@ -43,11 +56,25 @@
                 });
             },
             //通过userid来获取当前员工的日志
-            findDairyByUser(userId){
-                dairyService.findDairyByUser(this,userId,(data)=>{
-                    this.list=data;
+            findDairyByUser(){
+                var data = {
+                    per_page: this.pagination.per_page,
+                    current_page: this.pagination.current_page,
+                    userId: this.$route.params.userId
+                };
+                dairyService.findDairyByUser(this,data,(data)=>{
+                    this.list=data.data;
+                    this.pagination.total = data.sum;
+                    if(data.sum>this.pagination.per_page){
+                        this.pagination.last_page = (this.pagination.total % this.pagination.per_page) == 0 ? (this.pagination.total / this.pagination.per_page):(this.pagination.total / this.pagination.per_page)+1
+                    }else{
+                        this.pagination.last_page=1;
+                    }
                 })
             }
+        },
+        components: {
+            pagination: require('vue-bootstrap-pagination')
         }
     }
 </script>

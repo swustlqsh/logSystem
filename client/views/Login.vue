1afbb41c62785383a7a59660af0c44c77c7d30a4
刘娟娟 <1003512331@qq.com>
2016-07-15 09:35:29 +0800

    登录界面

--- client/views/Login.vue
+++ client/views/Login.vue
@@ -1,9 +1,17 @@
 <template>
     <div class="container">
-        <form class="form-signin">
-            <input type="email" placeholder="email" v-model="user.email">
-            <button  type="submit" @click="login()">登录</button>
-        </form>
+        <div class="login-header"></div>
+        <div class="login-logo">
+            <img src="../imgs/logo.png">
+        </div>
+        <div class="login-container">
+            <div class="login-form">
+                <h1 class="login-title">登录</h1>
+                <label for="email" class="login-label">工作邮箱</label>
+                <input type="text" name="email" class="login-txt" id="email">
+                <button class="login-btn" @click="login()">登录</button>
+            </div>
+        </div>
     </div>
 </template>
 <script type="text/ecmascript-6">
@@ -25,13 +33,5 @@
     }
 </script>
 <style>
-    .container {
-        height: 100%;
-        width: 100%;
-    }
-    .form-signin {
-        max-width: 330px;
-        padding: 150px;
-        margin: 0 auto;
-    }
+@import "../css/login.css";
 </style>

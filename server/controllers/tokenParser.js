2e34f37a1852865a425cdc86f7efed8b9a43e4cb
juan <1003512331@qq.com>
2016-07-17 14:09:21 +0800

    token

--- server/controllers/tokenParser.js
+++ server/controllers/tokenParser.js
@@ -1,6 +1,6 @@
 const Token = require('../models/Token');
 exports.parseToken = function (req, res, next) {
-        Token.count({token: req.query.token || req.body.token}, function (err, data) {
+        Token.count({token:req.headers.authorization||req.query.token || req.body.token}, function (err, data) {
             if (err) {
                 res.json({code: 555, data: 'token错误'});
                 res.end();
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -49,6 +49,7 @@ exports.findByTeam=(req,res)=>{
         });
 };*/
 exports.findById=(req,res)=>{
+    console.log(req.params._id)
     User.findById(req.params._id,(err,user)=>{
         if(err){
             res.json({code:555,data:err})
